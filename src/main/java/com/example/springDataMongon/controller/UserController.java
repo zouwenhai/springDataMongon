ce7a724b1ba4fd06f6c39177fedd905b3fd64aa8
zwh <1466625029@qq.com>
2020-01-21 17:06:15 +0800

    Initial commit

---
+++ src/main/java/com/example/springDataMongon/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.springDataMongon.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zouwenhai
+ * @version v1.0
+ * @date 2020/1/20 13:36
+ * @work //TODO
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+
+}
---
+++ src/test/java/com/example/springDataMongon/SpringDataMongonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDataMongon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
