43f99afa39402eca1d1daa6c362d1e3988dc4585
zwh <1466625029@qq.com>
2020-04-27 15:09:42 +0800

    mogodb

---
+++ src/main/java/com/example/springDataMongon/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.springDataMongon.service;
+
+import com.example.springDataMongon.entity.User;
+
+/**
+ * @author zouwenhai
+ * @version v1.0
+ * @date 2020/4/27 14:55
+ * @work //TODO
+ */
+public interface UserService {
+
+
+    public void saveUser(User user);
+}
